0c898e8291790bbd60530ceb2a709329b2876338
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-23 20:03:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -28,4 +28,18 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive ✅");
+});
+
+app.listen(PORT, () => {
+  console.log(`Keep-alive server running on port ${PORT}`);
+});
+
+
 client.login(TOKEN);
