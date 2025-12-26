f0c3198b2e50f430a7d19c0e66a84f50f5849097
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-26 16:00:15 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  TOKEN: process.env.DISCORD_TOKEN,
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
 
   SHEET_ID: process.env.SHEET_ID,
 
+  // ✅ DO NOT CHANGE THIS NAME AGAIN
+  GOOGLE_CREDS_BASE64: process.env.GOOGLE_CREDS_BASE64,
+
   LOG_ROLE_ID: process.env.LOG_ROLE_ID || "",
 
-  GOOGLE_CREDS_ENV: process.env.GOOGLE_CREDS_ENV || "GOOGLE_CREDS_BASE64",
+  PORT: process.env.PORT || 10000
 };
