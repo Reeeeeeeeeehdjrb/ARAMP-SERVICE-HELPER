60306ff02646ae71559e1f61398c6800fda6f8fe
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-26 13:25:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  TOKEN: process.env.DISCORD_TOKEN,
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
 
-  SHEET_ID: process.env.SHEET_ID, // make sure you add this in Render
-  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID || null,
+  SHEET_ID: process.env.SHEET_ID,
 
-  LOG_ROLE_ID: process.env.LOG_ROLE_ID || null,
+  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID,
+  LOG_ROLE_ID: process.env.LOG_ROLE_ID,
+
+  // Your screenshot shows this name:
+  GOOGLE_CREDS_BASE64: process.env.GOOGLE_CREDS_BASE64,
 
   PORT: process.env.PORT || 3000,
 };
