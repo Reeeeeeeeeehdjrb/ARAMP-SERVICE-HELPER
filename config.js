7c9b2e65478ba3b84e1db1e9c3d4727c46c97d4e
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-27 07:18:28 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,10 @@ module.exports = {
   GUILD_ID: process.env.GUILD_ID,
 
   SHEET_ID: process.env.SHEET_ID,
+
   LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID,
   LOG_ROLE_ID: process.env.LOG_ROLE_ID,
 
-  PORT: process.env.PORT || 3000,
+  // ✅ Keep your existing env var name
+  GOOGLE_CREDS_ENV: "GOOGLE_CREDS_BASE64",
 };
